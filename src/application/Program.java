be4a2426e68f79295749e0c1f81c9064fdc592f6
Neliton PS <neliton.net@hotmail.com>
2020-08-24 15:11:28 -0300

    Implementação classe SavingsAccount

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,41 @@
 package application;
 
+import entities.Account;
 import entities.BusinessAcount;
+import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-		BusinessAcount account = new BusinessAcount(8010, "Bob Brown", 10.0, 500.0);
+		//BusinessAcount account = new BusinessAcount(8010, "Bob Brown", 10.0, 500.0);
 
-		System.out.println("Balance: " + account.getBalance());
+		//System.out.println("Balance: " + account.getBalance());
+		
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinessAcount bacc = new BusinessAcount(1002, "Maria", 0.0, 500.0);
+		
+		//UPCASTING - é converter um objeto da subclasse para superclasse
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAcount(1003, "Bob", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+		
+		//DOWNCASTING - é converter um objeto da superclasse para subclasse
+		BusinessAcount acc4 = (BusinessAcount)acc2;//usa-se o casting para forçar converção
+		acc4.loan(100.0);
+		
+		//BusinessAcount acc5 = (BusinessAcount)acc3;
+		if(acc3 instanceof BusinessAcount) {
+			BusinessAcount acc5 = (BusinessAcount)acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		
+		if(acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
 		
 	}
 
